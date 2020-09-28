d20952923a9d5dd2e2fc73962b0fcdc31cfe01d1
George <george.ry.k@gmail.com>
2020-09-28 19:21:55 -0400

    Addin to search for imported function in corresponding file

--- R/sourcens.R
+++ R/sourcens.R
@@ -1,4 +1,4 @@
-pacman::p_load(namespace)
+pacman::p_load(namespace, magrittr, data.table)
 
 #' @title Load a file into a corresponding namespace.
 #' @description Load a file into a corresponding namespace.
@@ -27,4 +27,51 @@ sourcens <- function(path) {
   base::namespaceExport(ns, ls(ns))
 
   invisible()
+}
+
+#' navigation Addin:
+
+searchSourceNSFuncAddin <- function() {
+  pacman::p_load(data.table, magrittr, dplyr)
+
+  row <- rstudioapi::getActiveDocumentContext()$selection[[1]]$range$start[1] %>% unname
+  col <- rstudioapi::getActiveDocumentContext()$selection[[1]]$range$start[2] %>% unname
+
+  ss <- rstudioapi::getActiveDocumentContext()$contents[row]
+
+  ss1 <- substr(ss, 1, col)
+  ss2 <- substr(ss, col+1, nchar(ss))
+  sym <- paste0(strsplit(ss1, '[^A-Za-z:_]') %>% unlist %>% tail(1),
+                strsplit(ss2, '[^A-Za-z:_]') %>% unlist %>% head(1))
+  sym
+  # strToSearch <- deparse(eval(parse(text = sym))) %>% gsub('[[:space:]]', '', .) %>% paste(collapse = '[0-9]')
+  # strToSearch
+
+  strToSearchSimple <- paste0('[\n][0-9]+',
+                              strsplit(sym, '::') %>% unlist %>% .[2],
+                              '[\n0-9]*<-[\n0-9]*function[\n0-9]*[(]')
+  strToSearchSimple
+
+  searchFile <- paste0(strsplit(sym, '::') %>% unlist %>% .[1], '.R')
+  searchFile
+
+  if (!file.exists(searchFile)) {
+    return(invisible(0))
+  }
+
+  fileLines <- data.frame(Line = readLines(searchFile, warn=F)) %>% as.data.table
+  fileContents <-
+    # fileLines[, Line := paste0(.I, Line) %>% gsub('[[:space:]]', '', .)]$Line %>%
+    fileLines %>% mutate(Line = paste0(row_number(), Line) %>% gsub('[[:space:]]', '', .)) %>%
+    .$Line %>%
+    paste(collapse = '\n')
+  fileContents
+
+  pacman::p_load(stringr)
+
+  functLine <- str_extract(fileContents, strToSearchSimple)
+  rowNew <- functLine %>% str_extract('[0-9]+') %>% as.numeric()
+  #
+
+  rstudioapi::navigateToFile(searchFile, rowNew, 0)
 }
